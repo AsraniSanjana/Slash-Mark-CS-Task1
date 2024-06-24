584e82aab1b2c3772423408e42bcdf8abeea1f4f
Sanjana Asrani <109857762+AsraniSanjana@users.noreply.github.com>
2024-06-24 16:12:07 +0530

    aes decrypt fixed

--- T1-step2/AES.js
+++ T1-step2/AES.js
@@ -25,15 +25,34 @@ function encrypt(text, encryptionKey, iv) {
     return ivBuffer.toString('ascii') + ':' + encrypted.toString('hex');
 }
 
-function decrypt(text, encryptionKey) {
-    let textParts = text.split(':');
-    let iv = Buffer.from(textParts.shift(), 'hex');
-    let encryptedText = textParts.join(':');
 
-    let decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(encryptionKey), iv);
-    let decrypted = decipher.update(encryptedText, 'hex', 'utf8');
+function decrypt(encryptedText, encryptionKey, iv) {
+    let ivBuffer;
+
+    // Ensure IV is 16 ASCII characters
+    if (iv) {
+        if (iv.length !== 16) {
+            throw new Error('IV must be 16 ASCII characters long.');
+        }
+        ivBuffer = Buffer.from(iv, 'ascii');
+    } else {
+        throw new Error('IV is required for decryption.');
+    }
+
+    let textParts = encryptedText.split(':');
+    let ivFromText = Buffer.from(textParts.shift(), 'ascii');
+    if (!ivBuffer.equals(ivFromText)) {
+        throw new Error('IV mismatch.');
+    }
+
+    let encryptedData = textParts.join(':');
+    let decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(encryptionKey), ivBuffer);
+    let decrypted = decipher.update(encryptedData, 'hex', 'utf8');
     decrypted += decipher.final('utf8');
     return decrypted;
 }
 
 module.exports = { encrypt, decrypt };
+
+
+console.log(decrypt("1234567890123456:a4965fb520031b0081688590f9046a71", "1234567890abcdef1234567890abcdef", "1234567890123456"));
